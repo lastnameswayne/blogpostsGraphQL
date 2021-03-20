f3e1ba1a775a41ad5f451e4bbb376db5d0226eeb
lastnameswayne <tiarnan1109@gmail.com>
2021-03-20 19:03:28 +0100

    added authenthication

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,46 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
 import { GameResolver } from "./resolvers/GameResolver";
+import { UserResolver } from "./resolvers/UserResolver";
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis'
 
 (async () => {
   const app = express();
 
-  await createConnection();
+  const RedisStore = connectRedis(session)
+  const redisClient = redis.createClient()
+
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 10000000000, //long time
+        httpOnly: true,
+        secure: false,  //cookie only works in https (we are developing)
+        sameSite: 'lax'
+      },
+      saveUninitialized: false,
+      secret: 'qiwroasdjlasddde', //you would want to hide this in production
+      resave: false
+    })
+  )
+
+  const conn = await createConnection();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloWorldResolver, GameResolver]
+      resolvers: [HelloWorldResolver, GameResolver, UserResolver]
     }),
     context: ({ req, res }) => ({ req, res })
   });
+
+  conn.runMigrations()
 
   apolloServer.applyMiddleware({ app, cors: false });
 
